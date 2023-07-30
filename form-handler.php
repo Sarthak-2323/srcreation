ba23afe3d90cb6bc95a5f1177c64b3f76d097153
Sarthak-2323 <137101467+Sarthak-2323@users.noreply.github.com>
2023-07-30 10:35:15 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,14 +4,14 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+$mailTo = 'sarthak0723@gmail.com';
+
 $email_subject = 'New Form Submition';
 
 $email_body = "User Name: $name.\n".
                 "User email: $visitor_email.\n".
                   "Subject: $subject.\n".
                   "User Message: $message .\n";
-
-$to = 'sarthakraut2323@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
@@ -21,4 +21,15 @@ $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
+
+$result = mail($mailTo,$email_subject,$email_body,$headers);
+ 
+   //error checking
+   if($result) {
+    $success = "The message was sent successfully!";
+   } else {
+    $failed = "Error: Message was not sent, Try again Later";
+   }
+}
+ 
 ?>
